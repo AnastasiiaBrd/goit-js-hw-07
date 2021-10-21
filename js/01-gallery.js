2e7482d7c0e1e39ace999332bc83ad625fbbef00
AnastasiiaBrd <nastuxa1357@gmail.com>
2021-10-22 00:25:54 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+    </div>`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", getImageUrl);
+
+function getImageUrl(event) {
+  event.preventDefault();
+  const imageUrl = event.target.dataset.source;
+  let srcImage = instance.element().querySelector("img");
+  srcImage.setAttribute("src", imageUrl);
+  instance.show();
+}
+
+const instance = basicLightbox.create(
+  `
+    <img src="assets/images/image.png" width="800" height="600">
+`,
+  {
+    onShow: (instance) => {
+      instance.element().querySelector("img").onclick = instance.close;
+    },
+  }
+);
 
 console.log(galleryItems);
